8547b9ad21a23f72b4f1531e3b255ad59286acda
Anshul Shah <anshulshah96@gmail.com>
2016-09-29 23:52:46 +0530

    First Commit

---
+++ Computer Science - Job Scheduler/ans.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define inf 0x7fffffff
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define pr pair<int,int>
+#define mp(a,b) make_pair(a,b)
+#define pb push_back
+#define fr first
+#define sc second
+#define mset(arr,val) memset(arr,val,sizeof(arr));
+
+const int MAX = 1000005;
+const int MOD = 1e9+7;
+
+int main(){
+	freopen("input.txt","r",stdin);
+	
+}
